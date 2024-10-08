9cb99ccbb633d19fbaf9f501604b0f5c650838ee
pedrosiufc <pedrosiufc@gmail.com>
2024-10-08 17:03:55 -0300

    Instalado o modulo socket.io, e configurada conexão via WebSocket

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 /*importar as configurções do servidor*/
 var app = require('./config/server');
 //parametrizando a porta de escuta do servidor
-app.listen(80, function(){
+var server = app.listen(80, function(){
   console.log('Servidor online');
 })
+
+var io = require('socket.io').listen(server); //atribuindo a uma variável
+
+//criar a conexão por WebSocket
+
+io.on('connection', function(socket){
+  console.log('Usuário conectou!');
+
+  socket.on('disconnect', function(){
+    console.log('Usuário desconectado!');
+  });
+});
